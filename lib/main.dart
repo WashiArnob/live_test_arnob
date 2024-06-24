1b3f12499ccb9cc6d3b4e8b79765b88684f96542
Abu Washi Arnob <118329588+WashiArnob@users.noreply.github.com>
2024-06-24 23:28:33 +0600

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
+class Media {
+  void play() {
+    print('Playing media...');
+  }
+}
+
+class Song extends Media {
+  String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print('Playing song by $artist...');
+  }
+}
 
 void main() {
-  print('my world');
+  Media media = Media();
+  media.play();
+
+  Song song = Song('Artist Name');
+  song.play();
 }
