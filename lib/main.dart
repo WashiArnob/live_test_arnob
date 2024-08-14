251503c301a5906400a97fe03d17f27dbd4326e6
Abu Washi Arnob <118329588+WashiArnob@users.noreply.github.com>
2024-08-14 20:51:17 +0600

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,47 @@
-class Media {
-  void play() {
-    print('Playing media...');
-  }
-}
-
-class Song extends Media {
-  String artist;
-
-  Song(this.artist);
-
-  @override
-  void play() {
-    print('Playing song by $artist...');
-  }
-}
+import 'package:flutter/material.dart';
 
 void main() {
-  Media media = Media();
-  media.play();
+  runApp(MyApp());
+}
 
-  Song song = Song('Artist Name');
-  song.play();
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Greeting App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Hello, World!',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text('Welcome to Flutter!'),
+              Image.network(
+                'https://flutter.dev/assets/images/shared/brand/flutter/logo/flutter-lockup.png',
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.green,
+                ),
+                onPressed: () {
+                  const snackBar = SnackBar(content: Text('Button Pressed!'));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
+                child: const Text('Press me'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
